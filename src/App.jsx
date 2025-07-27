02f9174fbaec505d4c4fc5da0c7966ec6ce8ae85
HibaCodes <hibatae0@gmail.com>
2025-07-27 19:00:07 +0100

    Worked on the logic of the ressources section

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,14 @@ import Emplois from './components/emplois/Emplois'
 import Stages from './components/stages/Stages'
 import Enreprises from './components/entreprises/Entreprises'
 import Laureats from './components/laureats/Laureats'
-import Ressources from './components/ressources/Ressources'
+import RessourcesLayout from './components/ressources/RessourcesLayout'
+import OutilsPratiques from './components/ressources/outilsPratiques'
+import RscCertification from './components/ressources/RscCertification'
+import RscTextuelles from './components/ressources/RscTextuelles'
+import RscInteractives from './components/ressources/RscInteractives'
 import Login from './components/login/Login'
 import Signup from './components/signup/Signup'
+
 
 
 function App() {
@@ -24,7 +29,12 @@ function App() {
         <Route path="stages" element={<Stages/>}/>
         <Route path="entreprises" element={<Enreprises/>}/>
         <Route path="laureats" element={<Laureats />}/>
-        <Route path="ressources" element={<Ressources />}/>
+        <Route path="ressources" element={<RessourcesLayout/>}>
+          <Route path="textuelles" element={<RscTextuelles />} />
+          <Route path="interactives" element={<RscInteractives />} />
+          <Route path="outils" element={<OutilsPratiques />} />
+          <Route path="certification" element={<RscCertification />} />
+        </Route>      
         <Route path="se-connecter" element={<Login />}/>
         <Route path="nouveau-compte" element={<Signup />}/>
         <Route path="*" element={<div>404 Not found</div>}></Route>
---
+++ src/components/ressources/RessourcesLayout.jsx
@@ -0,0 +1,71 @@
+import { Outlet } from "react-router";
+import { Link } from "react-router";
+import { useState } from "react";
+
+export default function RessourcesLayout() {
+  const [activeTab, setActiveTab] = useState("textuelles");
+  const tabs = [
+    { key: "textuelles", label: "Ressources textuelles" },
+    { key: "interactives", label: "Ressources interactives" },
+    { key: "outils", label: "Outils pratiques" },
+    { key: "certification", label: "Ressources de certification" },
+  ];
+  return (
+    <>
+      <h1 className="text-xl font-bold mx-10 mt-25 mb-1">Ressources</h1>
+      <p className="text-gray-500 text-sm mx-10 mt-1.5">
+        Bénéficiez de notre collection de ressources variées pour enrichir votre
+        apprentissage et maximiser votre réussite académique
+      </p>
+
+      <div className="flex justify-between mx-25 mt-13 text-xs font-bold text-blue-500">
+        {tabs.map((tab) => (
+          <Link key={tab.key} to={`/ressources/${tab.key}`}>
+            <button
+              key={tab.key}
+              onClick={() => {
+                setActiveTab(tab.key);
+              }}
+              className={`px-4 py-2 transition duration-200
+            ${
+              activeTab === tab.key
+                ? "bg-blue-400 text-white rounded-t-md h-10 w-45"
+                : "text-blue-500 hover:text-blue-700 hover:cursor-pointer"
+            }`}
+            >
+              {tab.label}
+            </button>
+          </Link>
+        ))}
+      </div>
+
+      <div className="bg-blue-400 h-0.25 mt-0.2 mb-10"></div>
+
+      <div className="relative text-gray-600 flex flex-row justify-center mb-15">
+        <input
+          type="search"
+          name="search"
+          placeholder="Rechercher par titre, auteur, technologie..."
+          className="bg-white h-10 px-5 w-100 pr-10 rounded-full text-sm focus:outline-none border border-gray-400"
+        />
+        <button type="submit" className="relative right-9 bottom-1.5 mt-3 mr-4">
+          <svg
+            className="h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="Capa_1"
+            viewBox="0 0 56.966 56.966"
+            xmlSpace="preserve"
+            width="200px"
+            height="200px"
+          >
+            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg>
+        </button>
+      </div>
+      <Outlet />
+      <div className="mb-15"/>
+    </>
+  );
+}
